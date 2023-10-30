61e353199c5578a612931eb8dc2d2a320e637419
tk100823 <91471595+trungkien100823@users.noreply.github.com>
2023-10-30 16:08:41 +0700

    Add project files.

---
+++ BusinessObjects/Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObjects
+{
+    public partial class Service
+    {
+        public Service()
+        {
+            staff = new HashSet<staff>();
+        }
+
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public DateTime? LatestUpdate { get; set; }
+        public int? AmountOfTime { get; set; }
+        public double? Price { get; set; }
+        public string Status { get; set; }
+        public int? AdminId { get; set; }
+
+        public virtual Admin Admin { get; set; }
+        public virtual ICollection<staff> staff { get; set; }
+    }
+}
---
+++ StudentHouseProject/Form1.cs
@@ -0,0 +1,10 @@
+namespace StudentHouseProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
